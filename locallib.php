374cd71ae241d83dadaad650ec947b7a1768c581
Aaron Wells <aaronw@catalyst.net.nz>
2014-08-20 15:41:08 +1200

    Fix warning about get_context_instance() deprecated

--- locallib.php
+++ locallib.php
@@ -322,7 +322,7 @@ class assign_submission_mahara extends assign_submission_plugin {
         $responsedata = false;
         if (!is_enabled_auth('mnet')) {
             $error = get_string('authmnetdisabled', 'mnet');
-        } else if (!has_capability('moodle/site:mnetlogintoremote', get_context_instance(CONTEXT_SYSTEM), NULL, false)) {
+        } else if (!has_capability('moodle/site:mnetlogintoremote', context_system::instance())) {
             $error = get_string('notpermittedtojump', 'mnet');
         } else {
             // Set up the RPC request.
