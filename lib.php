f0376f4c1763ea1fa3a6baaea361646c2a93a3be
Aaron Wells <aaronw@catalyst.net.nz>
2014-09-12 10:52:08 +1200

    Removing unused file download function
    I think this was just left over from the initial version of this
    plugin being copied from the "file" assignsubmission plugin.

--- lib.php
+++ lib.php
@@ -24,53 +24,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 /**
- * Serves assignment submissions and other files.
- *
- * @param mixed $course course or id of the course
- * @param mixed $cm course module or id of the course module
- * @param context $context
- * @param string $filearea
- * @param array $args
- * @param bool $forcedownload
- * @return bool false if file not found, does not return if found - just send the file
- */
-function assignsubmission_mahara_pluginfile($course, $cm, context $context, $filearea, $args, $forcedownload) {
-    global $USER, $DB;
-
-    if ($context->contextlevel != CONTEXT_MODULE) {
-        return false;
-    }
-
-    require_login($course, false, $cm);
-    $itemid = (int)array_shift($args);
-    $record = $DB->get_record('assign_submission', array('id'=>$itemid), 'userid, assignment', MUST_EXIST);
-    $userid = $record->userid;
-
-    if (!$assign = $DB->get_record('assign', array('id'=>$cm->instance))) {
-        return false;
-    }
-
-    if ($assign->id != $record->assignment) {
-        return false;
-    }
-
-    // Check is users submission or has grading permission.
-    if ($USER->id != $userid and !has_capability('mod/assign:grade', $context)) {
-        return false;
-    }
-
-    $relativepath = implode('/', $args);
-
-    $fullpath = "/{$context->id}/assignsubmission_mahara/$filearea/$itemid/$relativepath";
-
-    $fs = get_file_storage();
-    if (!$file = $fs->get_file_by_hash(sha1($fullpath)) or $file->is_directory()) {
-        return false;
-    }
-    send_stored_file($file, 0, 0, true); // Download MUST be forced - security!
-}
-
-/**
  * Get the list of MNet hosts that we are allowed to retrieve Mahara pages from.
  * (Only hosts that subscribe to the correct flags)
  *
